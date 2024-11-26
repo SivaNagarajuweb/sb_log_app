0726443990be465d071b1cd53f00e63266bcd5a0
SivaNagaraju <99392149+SivaNagarajuweb@users.noreply.github.com>
2024-11-26 14:29:47 +0530

    Update MsgRestController.java

--- src/main/java/in/ashokit/rest/MsgRestController.java
+++ src/main/java/in/ashokit/rest/MsgRestController.java
@@ -7,18 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 import in.ashokit.service.MsgService;
 
 @RestController
-@RequestMapping("/api")
 public class MsgRestController {
-	
-	@Autowired
-	private MsgService msgService;
 	
 	@GetMapping("/welcome")
 	public String getMsg() {
-		int i = 10;
-		int j = 20;
-		int age = 16;
-		int k = 30;
 		return msgService.getWelcomeMsg();
 	}
 	
